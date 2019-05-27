7f8003ec0bd5b448532a56a5c16c59fbc1777ae5
Mahesh Murthy <mahesh@zastrin.com>
2019-05-27 13:31:12 -0400

    Updates to migration file and truffle config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const RandomGraphToken = artifacts.require("./RandomGraphToken");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(RandomGraphToken);
 };
+
